d8245d5d3c6a43271eea633d0dea913cfb46bf53
Soraya Sanchez <sorayasanchez0503@gmail.com>
2022-04-06 06:45:32 -0500

    horoscopo component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { HoroscopoComponent } from './components/horoscopo/horoscopo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'inicio', component: InicioComponent },
+  {path: 'horoscopo', component: HoroscopoComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CardComponent } from './components/card/card.component';
+import { HoroscopoComponent } from './components/horoscopo/horoscopo.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CardComponent,
+    HoroscopoComponent,
+    InicioComponent,
+    
   ],
   imports: [
     BrowserModule,
